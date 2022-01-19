0e660c7ffa09b8c1fdb92eaf3596994df2adf9d1
ALEXks <alexks179@gmail.com>
2022-01-19 10:43:52 +0300

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.Console;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -12,17 +12,18 @@ import java.nio.file.Paths;
 
 public class Main extends Application {
 
+    public static String version = "1.0";
     public static String StatDirPath = System.getProperty("user.dir") + "/statistics/";
 
-    public static String readFile(String path, Charset encoding)
-            throws IOException
+    public static String readFile(String path, Charset encoding) throws IOException
     {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, encoding);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception
+    {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
         Parent p = loader.load();
         primaryStage.setTitle("Анализатор статистик");
@@ -31,13 +32,12 @@ public class Main extends Application {
         scene.getStylesheets().add("bootstrap3.css");
         scene.getStylesheets().add("progressBar.css");
         primaryStage.setScene(scene);
-//        primaryStage.getIcons().add(new Image("/icon.png"));
         ((Controller)loader.getController()).initController(primaryStage);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
-
 }
